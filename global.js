c9b56f24a5eb8b61e300407005216b20ee188570
lmalottucsd <lmalott@ucsd.edu>
2025-10-17 14:37:06 -0700

    insert inside for loop

--- global.js
+++ global.js
@@ -30,14 +30,18 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  // next step: create link and add it to nav
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+  // Adjust relative URLs
+  if (!url.startsWith("http")) {
+    url = BASE_PATH + url;
+  }
+
+  // Insert link
+  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
 }
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
   : "/website/";         // GitHub Pages repo name
 
-if (!url.startsWith('http')) {
-  url = BASE_PATH + url;
-}
+

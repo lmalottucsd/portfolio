918ce8800a8fb714445d8d4b88ef82f2e0f16d06
lmalottucsd <lmalott@ucsd.edu>
2025-10-17 14:29:43 -0700

    auto nav

--- global.js
+++ global.js
@@ -14,21 +14,13 @@ function $$(selector, context = document) {
 //   currentLink?.classList.add('current');
 // }
 
-  <nav>
-  <ul>
-    <li><a href="index.html">Home</a></li>
-    <li><a href="projects/index.html">Projects</a></li>
-    <li><a href="contact/index.html">Contact</a></li>
-    <li><a href="cv/index.html">CV/Resume</a></li>
-    <li><a href="https://github.com/lmalottucsd" target="_blank">GitHub</a></li>
-  </ul>
-  </nav>
+
 
 let pages = [
   { url: 'index.html', title: 'Home' },
-  { url: 'projects/index.html', title: 'Projects' },
-  { url: 'contact/index.html', title: 'Contact' },
-  { url: 'cv/index.html', title: 'CV/Resume' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'cv/', title: 'CV/Resume' },
   { url: 'https://github.com/lmalottucsd', title: 'GitHub' }
 ];
 

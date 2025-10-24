5f5cfab50125d4476372044164fd0bab02d32574
lmalottucsd <lmalott@ucsd.edu>
2025-10-23 18:15:46 -0700

    adding files

--- global.js
+++ global.js
@@ -75,5 +75,16 @@ select.addEventListener('input', function(event) {
   localStorage.colorScheme = value;
 });
 
-
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    console.log(response)
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
 

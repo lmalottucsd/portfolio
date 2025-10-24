a7452fbc247e0e28b3ba9a82e01879f020b4a4ed
lmalottucsd <lmalott@ucsd.edu>
2025-10-24 12:13:59 -0700

    switched to relative

--- global.js
+++ global.js
@@ -6,7 +6,7 @@ function $$(selector, context = document) {
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/" 
-  : "/portfolio/"; 
+  : "/portfolio"; 
 
 let pages = [
   { url: 'index.html', title: 'Home' },

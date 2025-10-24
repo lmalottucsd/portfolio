9af758f89d7095777e46931e5c367c6c63941a05
lmalottucsd <lmalott@ucsd.edu>
2025-10-24 10:40:08 -0700

    adding projects container, get json from given url

--- global.js
+++ global.js
@@ -83,6 +83,8 @@ export async function fetchJSON(url) {
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
---
+++ projects/projects.js
@@ -0,0 +1,3 @@
+import { fetchJSON, renderProjects } from '../global.js';
+const projects = await fetchJSON('../lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
